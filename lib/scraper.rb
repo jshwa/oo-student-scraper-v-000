c1096a3094c294b6eb31e94c126eac4dd3a93e56
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-17 20:28:03 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,6 +22,8 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     profile_page = Nokogiri::HTML(File.read(profile_url))
 
+    profile = {}
+
     profile_page.css("div.social-icon-container a").each do |social|
       case social
       when /twitter.com/
