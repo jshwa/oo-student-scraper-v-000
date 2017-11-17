cae9116ed0cbc17e7a4964904863f49a78b26450
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-17 18:12:34 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    learn_students = Nokogiri::HTML(File.read(index_url))
+    learn_students = Nokogiri::HTML(open(index_url))
 
     students = {}
 
@@ -27,4 +27,4 @@ class Scraper
 
 end
 
-Scraper.scrape_index_page('fixtures/student-site/students/index.html')
+Scraper.scrape_index_page("./fixtures/student-site/students/index.html")
