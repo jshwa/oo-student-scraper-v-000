ddcab0abe9e8af696ea5294fdd708f697d8a3c90
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-17 23:28:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   def initialize(student_hash)
       student_hash.each do |name, info|
-        self.send "#{name}=", info
+        send "#{name}=", info
       end
       @@all << self
   end
