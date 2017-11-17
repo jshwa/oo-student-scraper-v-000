e1ab8cddb00c9ca84b5170b1b72e5a4304026d62
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-17 20:42:11 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -38,11 +38,8 @@ class Scraper
     end
 
     profile[:profile_quote] = profile_page.css("div.vitals-text-container div.profile-quote").text
-    binding.pry
-    profile[:bio] = profile_page.css("div.bio-content content-holder div.description-holder").text
+    profile[:bio] = profile_page.css("div.bio-content div.description-holder p").text
     profile
   end
 end
 
-Scraper.new
-Scraper.scrape_profile_page("fixtures/student-site/students/ryan-johnson.html")
