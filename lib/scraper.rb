fb21703f2136117f25789c83e2ae751cdc9bcb4a
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-17 20:34:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -40,7 +40,6 @@ class Scraper
 
     # profile[:profile_quote] = profile_page.css("div.vitals-text-container div.profile-quote").text
     # profile[:bio] = profile_page.css("div.bio-content content-holder div.description-holder").text
-    binding.pry
     profile
   end
 end
