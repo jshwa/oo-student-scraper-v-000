06ead89588f3cc913b867ebc8e7be9d2f255ed8d
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-17 04:25:59 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,24 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    learn_students = Nokogiri::HTML(open("index_url"))
+
+    students = {}
+
+    binding.pry
+
+    learn_students.css(div.student-card).each do |student|
+      students = {
+        name: 
+        location: 
+        profile_url:
+      }
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
