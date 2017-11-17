dc23cdef0be219b6518050ecd64757389a6880d4
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-17 19:40:25 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,8 +22,9 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     profile_page = Nokogiri::HTML(File.read(profile_url))
 
+    binding.pry
     profile = {
-      binding.pry
+
       twitter: profile_page.css("div.social-icon-container a")[0]['href'],
       # linkedin: profile_page.css("div.social-icon-container a")[1]['href'],
       # github: profile_page.css("div.social-icon-container a")[2]['href'],
