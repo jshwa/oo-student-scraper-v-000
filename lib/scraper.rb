47c91fb091e85b0b5047fbc8d3db74d0cccd17f2
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-17 18:36:22 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,11 +10,11 @@ class Scraper
     students = {}
 
     learn_students.css("div.student-card").each do |student|
-      students = {
+      students = [
         name: student.css("h4.student-name").text,
         location: student.css("p.student-location").text,
         profile_url: student.css("a").attribute("href").value
-      }
+      ]
     end
     students
   end
